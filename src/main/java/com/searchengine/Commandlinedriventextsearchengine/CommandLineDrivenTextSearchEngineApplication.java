ea9070f5b62b6fd41a6ad7bd2bf03aa547a6831c
rodolfosilva <rasilvap22@gmail.com>
2021-05-17 14:05:05 -0500

    feat: Added first project structure

---
+++ src/main/java/com/searchengine/Commandlinedriventextsearchengine/CommandLineDrivenTextSearchEngineApplication.java
@@ -0,0 +1,13 @@
+package com.searchengine.Commandlinedriventextsearchengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommandLineDrivenTextSearchEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommandLineDrivenTextSearchEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/searchengine/Commandlinedriventextsearchengine/CommandLineDrivenTextSearchEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.searchengine.Commandlinedriventextsearchengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommandLineDrivenTextSearchEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
