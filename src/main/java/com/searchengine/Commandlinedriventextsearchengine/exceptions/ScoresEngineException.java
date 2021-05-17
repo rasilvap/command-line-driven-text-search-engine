2d6131a68fc5ab3d5a198263ec4e2c206369174e
rodolfosilva <rasilvap22@gmail.com>
2021-05-17 15:55:33 -0500

    feat: Added exceptions package and classes.

---
+++ src/main/java/com/searchengine/Commandlinedriventextsearchengine/exceptions/ScoresEngineException.java
@@ -0,0 +1,12 @@
+package com.searchengine.Commandlinedriventextsearchengine.exceptions;
+
+class ScoresEngineException extends Exception {
+
+    ScoresEngineException(String message) {
+        super(message);
+    }
+
+    ScoresEngineException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
